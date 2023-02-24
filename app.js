1c99df17da1466bcb3a1e885bc403f92d0b3ff57
rivalcoba <ivan.rivalcoba@gmail.com>
2023-02-24 08:53:49 -0600

    🚧 agregando comentarios

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
+// We are creating the express instance
 var app = express();
 
 // view engine setup
+// We are delcaring the localization of the views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
