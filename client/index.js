fbcee079f1ee2c298bbc8ded690c152e8752db91
rivalcoba <ivan.rivalcoba@gmail.com>
2023-04-26 21:06:22 +0000

    🌟 Materializecss

--- client/index.js
+++ client/index.js
@@ -1,5 +1,14 @@
 // cargando los estilos
 /* eslint-disable no-console */
 import './styles/style.css';
+/* eslint-disable */
+// Importandoe Estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+/* eslint-enable */
 
-console.log('🎉 Estilos cargados correctamente 🎉');
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
+
+console.log('Webpack Working Middleware!!! 📦');
