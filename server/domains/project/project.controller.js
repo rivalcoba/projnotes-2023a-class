bb85dfd957585eeb523440fa804bccd4b63c87d7
rivalcoba <ivan.rivalcoba@gmail.com>
2023-06-01 14:36:17 +0000

    👌 View-model de validación

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -20,7 +20,17 @@ const addPost = (req, res) => {
   // se le informa al cliente
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
-    res.status(422).json(validationError);
+    // Se desestructuran los datos de validación
+    const { value: project } = validationError;
+    // Se extraen los campos que fallaron en la validación
+    const errorModel = validationError.inner.reduce((prev, curr) => {
+      // Creando una variable temporal para
+      // evitar el error "no-param-reassing"
+      const workingPrev = prev;
+      workingPrev[`${curr.path}`] = curr.message;
+      return workingPrev;
+    }, {});
+    res.status(422).json({ project, errorModel });
   } else {
     // En caso de que pase la validación
     // Se desestructura la información
