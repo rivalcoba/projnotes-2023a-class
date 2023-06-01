db30e12aacef0d15251c4bea73f8c02308318b62
rivalcoba <ivan.rivalcoba@gmail.com>
2023-06-01 13:21:50 +0000

    ✅ Validación con Yup

--- server/domains/project/project.validator.js
+++ server/domains/project/project.validator.js
@@ -1,6 +1,8 @@
 // Importando biblioteca de validacion
 import * as Yup from 'yup';
 
+import log from '../../config/winston';
+
 // Creando un esquema de validación para el proyecto
 const projectSchema = Yup.object().shape({
   name: Yup.string().required('Se requiere un nombre de proyecto'),
@@ -13,9 +15,17 @@ const projectSchema = Yup.object().shape({
 const getProject = (req) => {
   // Extrayendo datos de la petición
   const { name, description } = req.body;
+  log.info(
+    `Se extraen datos de la petición: name ${name}, description: ${description}`,
+  );
   // Regresando el objeto proyecto
   return {
     name,
     description,
   };
+};
+
+export default {
+  projectSchema,
+  getProject,
 };
--- server/services/validateFactory.js
+++ server/services/validateFactory.js
@@ -1,3 +1,6 @@
+// Importing winston logger
+import log from '../config/winston';
+
 // Usando el patrón Factory para la creación de un middelware de validación
 // lo que sigue es una función que regresa una función
 const Validator =
@@ -8,12 +11,15 @@ const Validator =
     // El proceso de validación se encierra en un bloque try
     try {
       // Se valida el objeto
+      log.info(`Inicia la validación de Project: ${typeof schema}`);
       const validData = await schema.validate(dataObject, {
         abortEarly: false,
       });
+      log.info('Pasa la validación de Project');
       // Se inyecta el objeto validado en la petición
       req.validData = validData;
     } catch (error) {
+      log.info(`Falla la validación de Project: ${error}`);
       // Creando objeto de validación
       req.errorData = error;
     }
